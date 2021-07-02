2baedea0681c783b914c635bdac7bcc79f6cdc8e
luigi2001 <luigitanga@virgilio.it>
2021-07-02 16:35:15 +0200

    add script + bonus

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 
+const app = new Vue(
+    {
+        el: '#app',
+        data:{
+            array: []
+        },
+        mounted(){
+            for(let i = 0; i < 10; i++){
+                axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(element => {
+                    this.array.push(element.data.response);
+                });
+            }
+        }
+    }
+);
